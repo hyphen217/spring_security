7cd5e45618ffaa98f036b02813754419e4fedf0e
DESKTOP-0KKV24T\USER <hyphen217@gmail.com>
2023-04-26 12:54:54 +0900

    트랜잭션의 이해 완료

--- src/main/java/edu/global/ex/controller/LoginController.java
+++ src/main/java/edu/global/ex/controller/LoginController.java
@@ -2,19 +2,30 @@ package edu.global.ex.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.global.ex.service.UserService;
+import edu.global.ex.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class LoginController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private PasswordEncoder encoder; // SecurityConfig.java의 Bean bCryptPasswordEncoder를 꺼내옴
 
 	@GetMapping("/login")
 	public String login() {
@@ -25,7 +36,7 @@ public class LoginController {
 	@RequestMapping(value = "/loginInfo", method = RequestMethod.GET)
 	public String loginInfo(Authentication authentication, Principal principal, Model model) {
 		// session에 저장된 시큐리티 객체에서 불러온다.(UserDetails 기반으로 만들었음)
-		//Controller함수에서만 받아올 수 있음
+		// Controller함수에서만 받아올 수 있음
 
 		String user_id;
 
@@ -40,6 +51,26 @@ public class LoginController {
 
 		// 3.Principal 객체로 가져오는 방법(가져올수 있는게 getName() 정도)
 		System.out.println("Principal 유저 아이디:" + principal.getName());
+
+		return "redirect:/";
+	}
+
+	// restful방식 /{id}/{pw}
+	@GetMapping("/addUser/{id}/{pw}") // url을 파라미터로 받겠다. @PathVariable에 {id}와 {pw}값을 넘김
+	public String addUser(@PathVariable String id, @PathVariable String pw) throws Exception {
+		System.out.println(id + ":" + pw);
+
+		UserVO user = new UserVO();
+		user.setEnabled(1);
+		user.setUsername(id);
+		user.setPassword(encoder.encode(pw.toString().trim())); // encoder : 암호화 객체
+
+		System.out.println(user);
+//		userService.addUser(user);
+//		userService.addUser2(user);	
+//		userService.addUser3(user);	
+//		userService.addUser4(user);	
+		userService.addUser5(user);	
 
 		return "redirect:/";
 	}
--- src/main/java/edu/global/ex/service/UserService.java
+++ src/main/java/edu/global/ex/service/UserService.java
@@ -1,7 +1,10 @@
 package edu.global.ex.service;
 
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.global.ex.mapper.UserMapper;
 import edu.global.ex.vo.UserVO;
@@ -13,8 +16,69 @@ public class UserService {
 	@Autowired
 	private UserMapper userMapper;
 
-	public int addUser(UserVO user) {
+	// 1.정상 동작 코드 => User를 insert를 넣은후 권한 까지 insert 해야 됨 =>
+	// 2개가 정상적인 동작이 되어야 됨(트랜잭션단위)
+	public void addUser(UserVO user) {
 		log.info("addUser()..");
-		return userMapper.insertUser(user);
+
+		// 반드시 같이 이루어져야하는 작업단위이므로 Service에서 함수로 묶음(트랜잭션)
+		userMapper.insertUser(user); // User를 insert를 넣은후
+		userMapper.insertAuthorities(user); // 권한 설정
+	}
+
+	// 2.아래는 select * from users; 에는 들어가 있으나
+	// select * from authorities 권한에는 안 들어가 있음
+	public void addUser2(UserVO user) {
+		log.info("addUser2()..");
+
+		userMapper.insertUser(user); // User를 insert를 넣은후
+		user = null; // 일부러 에러냄
+		userMapper.insertAuthorities(user); // 권한 설정
+	}
+
+	// 3. 아래처럼 @Transactional을 붙임 롤백되어 아무런 데이터가 들어가지않음
+	@Transactional // insertUser가 안된 상태로 되돌려라
+	public void addUser3(UserVO user) {
+		log.info("addUser3()..");
+
+		userMapper.insertUser(user); // User를 insert를 넣은후
+		user = null; // 일부러 에러냄
+		userMapper.insertAuthorities(user); // 권한 설정
+	}
+
+	// 4.checked Exception을 강제로 시켜 봄 //롤백 되지 않음
+	@Transactional
+	public void addUser4(UserVO user) throws Exception {
+		log.info("addUser()..");
+
+		userMapper.insertUser(user); // User를 insert를 넣은후
+		userMapper.insertAuthorities(user);
+
+		// throw Checked Exception
+		throw new Exception("Exception (Checked Exception)");
+	}
+
+	// 5. unChecked Exception을 강제로 시켜 봄 // 롤백 됨
+	@Transactional
+	public void addUser5(UserVO user) throws Exception {
+		log.info("addUser5()..");
+
+		userMapper.insertUser(user); // User를 insert를 넣은후
+		userMapper.insertAuthorities(user); // 해당 코드 때문에 롤백 됨
+
+		// throw unChecked Exception
+		throw new RuntimeException("RuntimeException (unChecked Exception)");
+	}
+
+	// ★추천 6. rollbackFor 옵션을 줌 // 롤백 됨
+	@Transactional(rollbackFor = Exception.class)
+	public void addUser6(UserVO user) throws Exception {
+		log.info("addUser()..");
+
+		userMapper.insertUser(user); // User를 insert를 넣은후
+		userMapper.insertAuthorities(user);
+
+		// throw Checked Exception
+		throw new Exception("RuntimeException (Unchecked Exception)");
 	}
 }
